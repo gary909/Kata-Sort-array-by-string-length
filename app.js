f8a963a9d208179a1f20caf81df13d3f495491a3
gary909 <garywhite61@hotmail.com>
2023-05-02 12:22:10 +0200

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function sortByLength (array) {
+    // Return an array containing the same strings, ordered from shortest to longest
+};
+
+console.log(sortByLength(["Beg", "Life", "I", "To"])); // ["I", "To", "Beg", "Life"]
+console.log(sortByLength(["", "Moderately", "Brains", "Pizza"])); // ["", "Pizza", "Brains", "Moderately"]
+console.log(sortByLength(["Longer", "Longest", "Short"])); // ["Short", "Longer", "Longest"]);
